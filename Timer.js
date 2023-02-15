b34794abcbd00b535b4e87ecfbd39c42783d7120
zmymrp <zmymrp@qq.com>
2023-02-15 16:51:23 +0800

    Ver5678
    当选择导入非当前学期时不计算开学日期

--- Timer.js
+++ Timer.js
@@ -8,29 +8,31 @@ async function scheduleTimer({
 		let times = information.times
 		let homepage = information.homepage
 		let startSemester = ''
-		let scripts = new DOMParser().parseFromString(homepage, 'text/html').getElementsByTagName('script')
-		let script = ''
-		for(i=0;i<scripts.length;i++){
-			if(typeof scripts[i].innerHTML != ''){
-				script += scripts[i].innerHTML
-			}
-		}
-		let dqjxz = script.match(/var.*dqjxz.*=.*\"(.*)\";/)
-		console.log("dqjxz=",dqjxz)
-		if(dqjxz.length == 2){
-			// 当前时间
-			let now = new Date();
-			let dqzs = Number(dqjxz[1]);
-			if(dqzs > 0){
-				// 获取第几周前的今天的时间戳
-				let tmp_time = now.getTime() - (86400000 * (dqzs - 1) * 7)
-				// 今天是星期几，0为星期天
-				if(now.getDay() == 0){
-					tmp_time -= 86400000 * 6
-				}else{
-					tmp_time -= 86400000 * (now.getDay() - 1)
+		if(homepage != '') {
+			let scripts = new DOMParser().parseFromString(homepage, 'text/html').getElementsByTagName('script')
+			let script = ''
+			for(i=0;i<scripts.length;i++){
+				if(typeof scripts[i].innerHTML != ''){
+					script += scripts[i].innerHTML
 				}
-				startSemester = tmp_time + ""
+			}
+			let dqjxz = script.match(/var.*dqjxz.*=.*\"(.*)\";/)
+			console.log("dqjxz=",dqjxz)
+			if(dqjxz != null && dqjxz.length == 2){
+				// 当前时间
+				let now = new Date();
+				let dqzs = Number(dqjxz[1]);
+				if(dqzs > 0){
+					// 获取第几周前的今天的时间戳
+					let tmp_time = now.getTime() - (86400000 * (dqzs - 1) * 7)
+					// 今天是星期几，0为星期天
+					if(now.getDay() == 0){
+						tmp_time -= 86400000 * 6
+					}else{
+						tmp_time -= 86400000 * (now.getDay() - 1)
+					}
+					startSemester = tmp_time + ""
+				}
 			}
 		}
 		let totalWeek = parserRes.something.max_number.weeks > 16?parserRes.something.max_number.weeks:30;
